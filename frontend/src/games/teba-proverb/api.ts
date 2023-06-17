c8e739121a58d795a446068633d3b79c30b454fa
migueLopez13 <mlopezariza1996@gmail.com>
2023-06-18 01:23:02 +0200

    perf: create enviroment files

--- frontend/src/games/teba-proverb/api.ts
+++ frontend/src/games/teba-proverb/api.ts
@@ -1,7 +1,9 @@
 import { proverb } from './definitions'
 
+const apiUrl = import.meta.env.VITE_API_URL
+
 export async function getProverb(): Promise<proverb> {
-  return await fetch('http://localhost:8001/api/proverbs/')
+  return await fetch(`http://${apiUrl}/api/proverbs/`)
     .then(async response => await response.json())
     .then(data => data)
 }
