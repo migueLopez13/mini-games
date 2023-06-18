4b30485aec23cddd75a82860a3cf292e2a06b818
migueLopez13 <mlopezariza1996@gmail.com>
2023-06-18 19:17:30 +0200

    add prod api url

--- frontend/src/games/teba-proverb/api.ts
+++ frontend/src/games/teba-proverb/api.ts
@@ -3,7 +3,7 @@ import { proverb } from './definitions'
 const apiUrl = import.meta.env.VITE_API_URL
 
 export async function getProverb(): Promise<proverb> {
-  return await fetch(`http://${apiUrl}/api/proverbs/`)
+  return await fetch(`${apiUrl}/api/proverbs/`)
     .then(async response => await response.json())
     .then(data => data)
 }
