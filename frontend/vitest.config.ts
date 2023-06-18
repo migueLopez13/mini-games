d50cac32a852574f0311a488ed48127ad99667dd
migueLopez13 <mlopezariza1996@gmail.com>
2023-06-18 17:59:42 +0200

    update vitest config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,10 @@
-import { configDefaults, defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig,mergeConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import viteConfig from './vite.config'
 
-export default defineConfig({
+export default mergeConfig(viteConfig,defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     exclude: [...configDefaults.exclude]
   }
-})
+}))
