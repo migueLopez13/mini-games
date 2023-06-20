626cee2b89eba3e538834733da5c5589b1e2cae4
Sergio Saez Trigueros <sergiosaez94@gmail.com>
2023-06-20 21:53:42 +0200

    sneaky log

--- frontend/src/plugins/supabase.ts
+++ frontend/src/plugins/supabase.ts
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_API_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-console.log(supabaseKey, supabaseUrl, import.meta.env.VITE_PROJECT_NAME)
 export const supabase = createClient(supabaseUrl, supabaseKey)
