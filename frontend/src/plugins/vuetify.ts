14a215cb07cde2156846f831bd12a95ced5a0be3
migueLopez13 <mlopezariza1996@gmail.com>
2023-06-24 00:50:39 +0200

    build: import only used vuetify components

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -4,18 +4,40 @@ import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import * as components from 'vuetify/components'
+import {
+  VBtn,
+  VDialog,
+  VIcon,
+  VLayout,
+  VMain,
+  VRow,
+  VCol,
+  VCard,
+  VImg,
+  VTextField
+} from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default createVuetify({
-  components,
+  components: {
+    VBtn,
+    VDialog,
+    VIcon,
+    VLayout,
+    VMain,
+    VRow,
+    VCol,
+    VCard,
+    VImg,
+    VTextField
+  },
   directives,
   icons: {
     defaultSet: 'mdi',
     aliases,
     sets: {
-      mdi,
-    },
+      mdi
+    }
   },
   theme: {
     defaultTheme: 'customTheme',
@@ -26,9 +48,9 @@ export default createVuetify({
           background: '#FFFFFF',
           primary: '#086788',
           secondary: '#06aed5',
-          accent: '#F0C808',
-        },
-      },
-    },
-  },
+          accent: '#F0C808'
+        }
+      }
+    }
+  }
 })
