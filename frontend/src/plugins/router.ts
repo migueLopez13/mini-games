765a35babb748c5135ffae6b087654499871451a
migueLopez13 <mlopezariza1996@gmail.com>
2023-06-24 00:26:18 +0200

    refactor: remove word search folder

--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -1,10 +1,8 @@
 import ProverbGameView from '../games/teba-proverb/game-view.vue'
-import WordSearchGameView from '../games/word-search/game-view.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   { path: '/proverbs', component: ProverbGameView },
-  { path: '/word-search', component: WordSearchGameView },
   { path: '/', component: ProverbGameView }
 ]
 
